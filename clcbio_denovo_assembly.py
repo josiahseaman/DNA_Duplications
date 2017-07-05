ddba61fd4dc78fd57b9b7e3c0594bba542ccaa22
Josiah Seaman <josiah@newline.us>
2017-07-05 20:20:06 +0100

    local import of paired reads

--- clcbio_denovo_assembly.py
+++ clcbio_denovo_assembly.py
@@ -1,21 +1,35 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 from __future__ import print_function
 
 import os
 from glob import glob
-from read_trimming import call, call_output
+import subprocess
+
+
+def call_output(args):
+    command = ' '.join(args) if isinstance(args, list) else args
+    print(command)
+    return subprocess.check_output(command, shell=True)
+
+
+def call(args):
+    command = ' '.join(args) if isinstance(args, list) else args
+    print(command)
+    return subprocess.call(command, shell=True)
+    # TODO: add error handling for bad return code
 
 
 def url(name):
     """Retrieves the CLC resource URL for a specific imported file.  Fails if the file is not imported"""
-    return name
+    return clc_server_root + os.path.basename(name)
 
 
-read_folder = '/home/jseaman/sequences/'  # TODO: needs to be filtered by FRAX number
+read_folder = '/home/jseaman/sequences/'
 output_dir = '/home/jseaman/assemblies/'
 server_cmd_path = "/usr/local/bin/clcserver"
 parse_cmd = "/usr/local/bin/clcresultparser"
+clc_server_root = 'clc://10.65.1.101:7777/clcserver/Fraxinus/'
 password_token = 'BAAAAAAAAAAAAAP5d9b3a807f99338e--260f6b54-15d0dbc6aad--8000'
 server_cmd = server_cmd_path + ' -S atta1.ad.kew.org -P 7777 -U jseaman -W %s ' % password_token
 
@@ -30,27 +44,48 @@ def make_clc_directory(subdirectory):
                        -p "-d" \
                        -c $subdirectory`
                        """
-    call([server_cmd, '-A mkdir'
-                      '-n', subdirectory,
-          '-t', "clc://server"
-                '-O mkdir_result.txt'])
+    call([server_cmd, '-A mkdir',
+          '-n', subdirectory,
+          '-t', clc_server_root,
+          '-O mkdir_result.txt'])
+    """Example output from the above command:
+    Message: Trying to log on to server
+    Message: Login successful
+    Message: Executing command mkdir
+    Name: FRAX14
+    Type: Directory
+    ClcUrl: clc://localhost:7777/1753281757-WAAAAAAAAAAAAAP695532d5ba6ceebf-30223f7e-15c77a61578--7fff/-298049416-HBAAAAAAAAAAAAPb7f807af1cc25381--21537d00-15c7851a624--7fff
+    ClcUrl Simple: clc://server/clcserver/Fraxinus/FRAX14
+    //"""
+
     return call_output([parse_cmd,
                         '-f mkdir_result.txt',
-                        '-p "-d"',
                         '-c', subdirectory])
+    # Example result: clcresultparser -f mkdir_result -c FRAX14
+    # clc://localhost:7777/1753281757-WAAAAAAAAAAAAAP695532d5ba6ceebf-30223f7e-15c77a61578--7fff/-298049416-HBAAAAAAAAAAAAPb7f807af1cc25381--21537d00-15c7851a624--7fff
 
 
 def import_pair(R1, R2, directory_url):
     """Imports an illumina mate pair reads with paths R1 and R2.
     Files are made available in CLC folder @directory_url
     http://resources.qiagenbioinformatics.com/manuals/clcservercommandlinetools/current/index.php?manual=ngs_import_illumina.html"""
+    print("Importing", R1, R2)
+    # First add the local files into the CLC server URL index system
+    call([server_cmd, '-A import',
+          '-s ', R1,
+          '--destination', clc_server_root,
+          '-f externalfile'])  # result can be retrieved with url(R1)
+    call([server_cmd, '-A import',
+          '-s ', R2,
+          '--destination', clc_server_root,
+          '-f externalfile'])  # result can be retrieved with url(R2)
+    # Then read in the data
     call([server_cmd, '-A ngs_import_illumina',
           '--paired-reads true',  # <Boolean>	Paired-end reads (default: false)
           '--read-orientation FORWARD_REVERSE',  # Read orientation (default: FORWARD_REVERSE)
-          ('--destination ' + directory_url),  # <ClcServerObjectUrl>	Destination file or folder on server.
-          '--log fasle',
-          '-f', R1,
-          '-f', R2])
+          '--destination ', directory_url,  # <ClcServerObjectUrl>	Destination file or folder on server.
+          '-f', url(R1),
+          '-f', url(R2)])
 
 
 def import_read_pairs_by_FRAX(frax_name):
@@ -87,7 +122,8 @@ def denovo_assembly(reads_directory):
     perform_scaffolding = '--perform-scaffolding false'
     # <Boolean>	Perform scaffolding based on paired reads. If the input data does not contain paired data,
     # scaffolding cannot be selected. (default: true)
-    assembly_url = call_output([server_cmd, auto_detect_paired_distances, bubblesize, input_folder,
+    assembly_url = call_output([server_cmd, '-A denovo_assembly',
+                                auto_detect_paired_distances, bubblesize, input_folder,
                                 minimum_contig_length, perform_scaffolding])
     return assembly_url
 
@@ -97,5 +133,9 @@ def main():
     # stage all the data to be used
     read_directory = import_read_pairs_by_FRAX(frax_number)
     # do de novo assembly using all info
-    assembly_url = denovo_assembly(read_directory)
-    print('Done with Assembly ', frax_number, assembly_url)
+    # assembly_url = denovo_assembly(read_directory)
+    print('Done with Assembly ', frax_number, read_directory)#assembly_url)
+
+
+if __name__ == '__main__':
+    main()
