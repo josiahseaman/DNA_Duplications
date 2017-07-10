48ece265ce4c601dab72daed4f0bcea490c9dc7d
Josiah Seaman <josiah@newline.us>
2017-07-10 11:12:55 +0100

    WIP:Rsync.  Refactored strip_frax()

--- clcbio_denovo_assembly.py
+++ clcbio_denovo_assembly.py
@@ -96,15 +96,20 @@ def import_pair(R1, R2, directory_url, frax_name):
               ])
 
 
+def strip_frax(frax_name):
+    """strips the 01 if it's there"""
+    return str(int(frax_name.replace('FRAX', '')))
+
+
 def import_read_pairs_by_FRAX(frax_name):
     # create directory to stage all relevant read pairs in
     directory_url = make_clc_directory(frax_name)
     # import all reads as pairs (where appropriate) for one particular frax_name
     relevant_files = glob(os.path.join(read_folder, frax_name) + '*')
-    stripped_number = str(int(frax_name.replace('FRAX', '')))  # strips the 01 if it's there
+    stripped_number = strip_frax(frax_name)
     relevant_files += glob(os.path.join(read_folder, stripped_number) + '_*')  # The NextSeq reads don't
     # have a proper FRAX01 prefix.  Renaming the files would allow you to remove this line
-    first_pairs = [x for x in relevant_files if '_R1_' in x]
+    first_pairs = [x for x in relevant_files if '_R1_' in x and 'ssembly' not in x]
     for R1 in first_pairs:
         R1 = os.path.join(read_folder, os.path.basename(R1))
         R2 = R1.replace('R1', 'R2')
@@ -132,9 +137,10 @@ def denovo_assembly(reads_directory_url):
     # <Boolean>	Determine appropriate paired distances as part of the assembly process. This is done
     # individually for each data set. If this option is not selected, the assembler will use the paired
     # distance information specified on the input data. (default: true)
-    bubblesize = '--bubblesize 5000'
+    bubblesize = '--bubblesize 5000 --bubblesize-auto false'
     # Integer: 1 <= x <= 5000	Bubble size used in the de Bruijn-Graph. Increase this when expecting many
     # SNP's or systematic errors. (default: 50)
+    mapping_type = '--map-reads-to-contigs SIMPLE'
 
     # destination = '--destination ' + url(output_dir)
     # <ClcServerObjectUrl> Destination file or folder on server. If not specified the folder of the first
@@ -149,13 +155,34 @@ def denovo_assembly(reads_directory_url):
     assembly_url = call_output([server_cmd, '-A denovo_assembly',
                                 auto_detect_paired_distances,
                                 bubblesize,
+                                mapping_type,
                                 minimum_contig_length,
                                 perform_scaffolding,
                                 input_files_string, ])
     return assembly_url
 
 
+def rsync_sequences(frax_number):
+    """Download data from QMUL"""
+    #TODO: untested communicate code
+    password = input(prompt='Enter password for btx142@frontend5.apocrita.hpc.qmul.ac.uk')
+    p = subprocess.Popen(
+        ["rsync -au --exclude '*singleton*' ",
+         "btx142@frontend5.apocrita.hpc.qmul.ac.uk:/data/SBCS-BuggsLab/LauraKelly/HiSeq_data"
+         "/trimmed_reads/%s_*" % frax_number,
+         "/home/jseaman/sequences/"], stdin=subprocess.PIPE)
+    p.communicate(input=password + '\n')
+
+    f = subprocess.Popen(
+        ["rsync -au --exclude '*singleton*' ",
+         "btx142@frontend5.apocrita.hpc.qmul.ac.uk:/data/SBCS-BuggsLab/LauraKelly/NextSeq_data"
+         "/trimmed_reads/%s_*" % strip_frax(frax_number),
+         "/home/jseaman/sequences/"], stdin=subprocess.PIPE)
+    f.communicate(input=password + '\n')
+
+
 def main(frax_number):
+    # rsync_sequences(frax_number)
     # stage all the data to be used
     reads_directory_url = import_read_pairs_by_FRAX(frax_number)
     # reads_directory_url = "clc://10.65.1.101:7777/clcserver/Fraxinus/FRAX01"
