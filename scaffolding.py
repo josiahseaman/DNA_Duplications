2a53cb37e29ba11e0c4c8c30036195fd6d4e3714
Josiah Seaman <josiah@newline.us>
2017-10-31 13:56:16 +0000

    added 'extend' option for scaffolding.  Added GapCloser config file example

--- scaffolding.py
+++ scaffolding.py
@@ -28,7 +28,7 @@ def call(args):
     return subprocess.check_output(command, shell=True)
 
 
-def SSPACE_scaffolding(frax_number, assembly_name, assembly_path):
+def SSPACE_scaffolding(frax_number, assembly_name, assembly_path, do_extension=False):
     """
     /data/home/eex057/SSPACE-STANDARD-3.0_linux-x86_64/SSPACE_Standard_v3.0.pl \
     	-s /data/SBCS-BuggsLab/LauraKelly/assembly/FRAX01_all_paired_reads_min_50bp_de_novo_assembly_max_bubble_size_5000_min_contig_length_200_contig_list.fa \ # contigs
@@ -45,12 +45,11 @@ def SSPACE_scaffolding(frax_number, assembly_name, assembly_path):
           '-s', assembly_path,  # contigs
           '-l /data/SBCS-BuggsLab/Josiah/DNA_Duplications/data/sspacelibraryfile_' + frax_number ,
           '-b ', assembly_name,  # output folder to create
+          '-x 1' if do_extension else '',  # extends read ends, added by Josiah
           '-T 24',  # -T = threads'])
           '2>&1 > "' + out_log_file_name(assembly_name) + '"',
           ])
     print("Done Scaffolding")
-
-    return os.path.join(output_dir, assembly_name, assembly_name + '.final.scaffolds.fasta')
 
 
 def gap_closer(frax_number, name, scaffold_path):
@@ -66,32 +65,39 @@ def gap_closer(frax_number, name, scaffold_path):
     call(['cd', output_dir])
     call(['GapCloser',
           '-a', scaffold_path,
-          '-b /data/SBCS-BuggsLab/Josiah/DNA_Duplications/data/' + frax_number + '-GapCloser01.config',
+          '-b /data/SBCS-BuggsLab/Josiah/DNA_Duplications/data/' + frax_number + '-GapCloser.config',
           '-o', name + '__GAPCLOSER',
           '-t 8',
           '> /data/SBCS-BuggsLab/Josiah/scaffolding/' + name + '__gapcloser.log 2>&1',
           ])
 
 
-def main(frax_number, name, path):
+def main(frax_number, assembly_name, path, options):
     # Get CLC denovo assembly
 
 
     # Scaffold using long mate pairs
-    scaffold_path = SSPACE_scaffolding(frax_number, name, path)
+    scaffold_path = os.path.join(output_dir, assembly_name, assembly_name + '.final.scaffolds.fasta')
+    SSPACE_scaffolding(frax_number, assembly_name, path, 'extend' in options)
+    print(scaffold_path)
 
     # Close gaps in scaffolded assembly
-    # gap_closer(frax_number, name, scaffold_path)
+    # gap_closer(frax_number, assembly_name, scaffold_path)
 
 
 if __name__ == '__main__':
-    assert len(sys.argv) == 4, "Include Frax number, Assembly name, Input path: e.g. FRAX01 FRAX01__2017-07-10 /data/SBCS-BuggsLab/Josiah/assembly/FRAX01__2017-07-10__bubblesize_5000__mincontig_200__SIMPLE__assembly.fa"
+    if len(sys.argv) < 4:
+        print("Include Frax number, Assembly name, Input path: e.g. FRAX01 FRAX01__2017-07-10 /data/SBCS-BuggsLab/Josiah/assembly/FRAX01__2017-07-10__bubblesize_5000__mincontig_200__SIMPLE__assembly.fa")
+        exit(1)
     frax_number, name, path = sys.argv[1], sys.argv[2], sys.argv[3]
+    options = []
+    if len(sys.argv) >= 5:
+        options = sys.argv[4]  # include 'extend' for SSPACE extension
     if frax_number == 'FRAX':
         print("You forgot to label your FRAX number (first argument) e.g. scaffold.py FRAX03")
         exit(1)
     log_file_name = frax_number + ' ' + log_file_name
-    main(frax_number, name, path)
+    main(frax_number, name, path, options)
 
 
 """
@@ -102,5 +108,9 @@ cd /data/scratch/eex057/FRAX01/
 GapCloser -a /data/scratch/eex057/FRAX01/RUN1/RUN1.final.scaffolds.fasta \
 	-b /data/scratch/eex057/FRAX01/GapCloser01.config \
 	-o GAPCLOSER01 \
-        -t 8 >log-gapcloser01.txt 2>&1 
+        -t 8 >log-gapcloser01.txt 2>&1
+        
+        
+GapCloser -a /data1/SBCS-BuggsLab/Josiah/scaffolding/FRAX09_Oct2017_draft_80GB/FRAX09_Oct2017_draft_80GB.final.scaffolds.fasta -b /data/SBCS-BuggsLab/Josiah/DNA_Duplications/data/FRAX09-GapCloser.config -o FRAX09_Oct2017_draft_80GB__GAPCLOSER -t 8 > /data/SBCS-BuggsLab/Josiah/scaffolding/FRAX09_Oct2017_draft_80GB__gapcloser.log 2>&1
+         
 """
