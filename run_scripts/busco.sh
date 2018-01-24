53db4c981aed0457783a0683f67db066ab24bdb3
Josiah Seaman <josiah@newline.us>
2018-01-24 16:44:52 +0000

    Busco for 1,2 6,7, 11

--- run_scripts/busco.sh
+++ run_scripts/busco.sh
@@ -17,13 +17,13 @@ export AUGUSTUS_CONFIG_PATH=/data/SBCS-BuggsLab/Josiah/DNA_Duplications/busco_co
 
 
 #RUN WITH WITH BATG0.5
-BUSCO.py --in /data/SBCS-BuggsLab/LauraKelly/ash_reference_genome/BATG-0.5_updated_organellar_scaffolds_PhiX_removed_new_copy.fa --out BATG_BUSCO --lineage /data/home/btw437/embryophyta_odb9 --mode genome -c 8 -e 1e-05 -sp tomato
+#BUSCO.py --in /data/SBCS-BuggsLab/LauraKelly/ash_reference_genome/BATG-0.5_updated_organellar_scaffolds_PhiX_removed_new_copy.fa --out BATG_BUSCO --lineage /data/home/btw437/embryophyta_odb9 --mode genome -c 8 -e 1e-05 -sp tomato
 
 #RUN WITH WITH FRAX01
-  #BUSCO.py --in /data/SBCS-BuggsLab/Josiah/scaffolding/FRAX01_800bp_HiSeq2500/FRAX01_GAPCLOSER__cat_merged.fa --out FRAX01_BUSCO --lineage /data/home/btw437/embryophyta_odb9 --mode genome -c 8 -e 1e-05 -sp tomato
+BUSCO.py --in /data/SBCS-BuggsLab/Josiah/scaffolding/FRAX01_800bp_HiSeq2500/FRAX01_GAPCLOSER__cat_merged.fa --out FRAX01_BUSCO --lineage /data/home/btw437/embryophyta_odb9 --mode genome -c 8 -e 1e-05 -sp tomato
 
 #RUN WITH WITH FRAX02
-  #BUSCO.py --in /data/SBCS-BuggsLab/Josiah/scaffolding/FRAX02_800bp_HiSeq2500/FRAX02_GAPCLOSER__cat_merged.fa --out FRAX02_BUSCO --lineage /data/home/btw437/embryophyta_odb9 --mode genome -c 8 -e 1e-05 -sp tomato
+BUSCO.py --in /data/SBCS-BuggsLab/Josiah/scaffolding/FRAX02_800bp_HiSeq2500/FRAX02_GAPCLOSER__cat_merged.fa --out FRAX02_BUSCO --lineage /data/home/btw437/embryophyta_odb9 --mode genome -c 8 -e 1e-05 -sp tomato
 
 #RUN WITH WITH FRAX03
 #BUSCO.py --in /data/SBCS-BuggsLab/Josiah/scaffolding/FRAX03_800bp_HiSeq2500/FRAX03_GAPCLOSER__cat_merged.fa --out FRAX03_BUSCO --lineage /data/home/btw437/embryophyta_odb9 --mode genome -c 8 -e 1e-05 -sp tomato
@@ -35,22 +35,22 @@ BUSCO.py --in /data/SBCS-BuggsLab/LauraKelly/ash_reference_genome/BATG-0.5_updat
 #BUSCO.py --in /data/SBCS-BuggsLab/Josiah/scaffolding/FRAX05_800bp_HiSeq2500/FRAX05_GAPCLOSER__cat_merged.fa --out FRAX05_BUSCO --lineage /data/home/btw437/embryophyta_odb9 --mode genome -c 8 -e 1e-05 -sp tomato
 
 #RUN WITH WITH FRAX06
-  #BUSCO.py --in /data/SBCS-BuggsLab/Josiah/scaffolding/FRAX06_800bp_HiSeq2500/FRAX06_32_hour_stall_merged.fa --out FRAX06_BUSCO_partial --lineage /data/home/btw437/embryophyta_odb9 --mode genome -c 8 -e 1e-05 -sp tomato
+BUSCO.py --in /data/SBCS-BuggsLab/Josiah/scaffolding/FRAX06_800bp_HiSeq2500/FRAX06_32_hour_stall_merged.fa --out FRAX06_BUSCO_partial --lineage /data/home/btw437/embryophyta_odb9 --mode genome -c 8 -e 1e-05 -sp tomato
 
 #RUN WITH WITH FRAX07
-  #BUSCO.py --in /data/SBCS-BuggsLab/Josiah/scaffolding/FRAX07_800bp_HiSeq2500/FRAX07_GAPCLOSER__cat_merged.fa --out FRAX07_BUSCO --lineage /data/home/btw437/embryophyta_odb9 --mode genome -c 8 -e 1e-05 -sp tomato
+BUSCO.py --in /data/SBCS-BuggsLab/Josiah/scaffolding/FRAX07_800bp_HiSeq2500/FRAX07_GAPCLOSER__cat_merged.fa --out FRAX07_BUSCO --lineage /data/home/btw437/embryophyta_odb9 --mode genome -c 8 -e 1e-05 -sp tomato
 
 #RUN WITH WITH FRAX08
 #BUSCO.py --in /data/SBCS-BuggsLab/Josiah/scaffolding/FRAX08_800bp_HiSeq2500/FRAX08_GAPCLOSER__cat_merged.fa --out FRAX08_BUSCO --lineage /data/home/btw437/embryophyta_odb9 --mode genome -c 8 -e 1e-05 -sp tomato
 
 #RUN WITH WITH FRAX09
-  #BUSCO.py --in /data/SBCS-BuggsLab/Josiah/scaffolding/FRAX09_Oct2017_unfiltered/Pennsylvanica_gapcloser_merge_unfiltered_with_bwa.fa --out FRAX09_BUSCO --lineage /data/home/btw437/embryophyta_odb9 --mode genome -c 8 -e 1e-05 -sp tomato
+BUSCO.py --in /data/SBCS-BuggsLab/Josiah/scaffolding/FRAX09_Oct2017_unfiltered/Pennsylvanica_gapcloser_merge_unfiltered_with_bwa.fa --out FRAX09_BUSCO --lineage /data/home/btw437/embryophyta_odb9 --mode genome -c 8 -e 1e-05 -sp tomato
 
 #RUN WITH WITH FRAX10
 #BUSCO.py --in /data/SBCS-BuggsLab/Josiah/scaffolding/FRAX10_800bp_HiSeq2500/FRAX10_GAPCLOSER__cat_merged.fa --out FRAX10_BUSCO --lineage /data/home/btw437/embryophyta_odb9 --mode genome -c 8 -e 1e-05 -sp tomato
 
 #RUN WITH WITH FRAX11
-  #BUSCO.py --in /data/SBCS-BuggsLab/Josiah/scaffolding/FRAX11_800bp_HiSeq2500/FRAX11_GAPCLOSER__cat_merged.fa --out FRAX11_BUSCO --lineage /data/home/btw437/embryophyta_odb9 --mode genome -c 8 -e 1e-05 -sp tomato
+BUSCO.py --in /data/SBCS-BuggsLab/Josiah/scaffolding/FRAX11_800bp_HiSeq2500/FRAX11_GAPCLOSER__cat_merged.fa --out FRAX11_BUSCO --lineage /data/home/btw437/embryophyta_odb9 --mode genome -c 8 -e 1e-05 -sp tomato
 
 #RUN WITH WITH FRAX12
 #BUSCO.py --in /data/SBCS-BuggsLab/Josiah/scaffolding/FRAX12_800bp_HiSeq2500/FRAX12_GAPCLOSER__cat_merged.fa --out FRAX12_BUSCO --lineage /data/home/btw437/embryophyta_odb9 --mode genome -c 8 -e 1e-05 -sp tomato
