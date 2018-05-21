755a6fa17a462defc710fd2f890e2fa923f503d5
Josiah Seaman <j.seaman@qmul.ac.uk>
2018-05-21 22:04:35 +0100

    tblastn -num_threads 4 -query ${root}/cds-parts.fasta

--- GeMoMa-1.5/FRAX32/FRAX32_qsub.sh
+++ GeMoMa-1.5/FRAX32/FRAX32_qsub.sh
@@ -18,7 +18,7 @@ GEMOMA=/data/SBCS-BuggsLab/Josiah/DNA_Duplications/GeMoMa-1.5/GeMoMa-1.5.jar
 makeblastdb -out ${out}/blastdb -hash_index -in ${target} -title "target" -dbtype nucl
 echo "tblastn:~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 #running BLAST search; have kept all parameter settings the same as the example given in run.sh, apart from the e-value (for which I changed it from 100 to 1e-5) + using multiple threads
-tblastn -num_threads 4 -query ${out}/cds-parts.fasta -db ${out}/blastdb -evalue 1e-5 -out ${out}/tblastn.txt -outfmt "6 std sallseqid score nident positive gaps ppos qframe sframe qseq sseq qlen slen salltitles" -db_gencode 1 -matrix BLOSUM62 -seg no -word_size 3 -comp_based_stats F -gapopen 11 -gapextend 1
+tblastn -num_threads 4 -query ${root}/cds-parts.fasta -db ${out}/blastdb -evalue 1e-5 -out ${out}/tblastn.txt -outfmt "6 std sallseqid score nident positive gaps ppos qframe sframe qseq sseq qlen slen salltitles" -db_gencode 1 -matrix BLOSUM62 -seg no -word_size 3 -comp_based_stats F -gapopen 11 -gapextend 1
 
 echo "GeMoMa:~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 java -jar ${GEMOMA} CLI GeMoMa t=${out}/tblastn.txt c=${root}/cds-parts.fasta a=${root}/assignment.tabular tg=${target} outdir=${out}
