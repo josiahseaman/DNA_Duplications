30ba0e1d6b21da6ba5d3531d03d24aafbb5e97ea
Josiah Seaman <josiah@newline.us>
2018-05-29 13:26:16 +0100

    Switching to Array job style

--- GeMoMa-1.5/gene_validator/validator_qsub.sh
+++ GeMoMa-1.5/gene_validator/validator_qsub.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#$ -cwd              		 # use current working directory
 #$ -V                		 # this makes it verbose
 #$ -m aes
 #$ -M josiah.seaman@gmail.com
@@ -18,31 +19,30 @@ echo $1 $2
 
 GEM_HOME="/data2/SBCS-BuggsLab/Josiah/DNA_Duplications/GeMoMa-1.5/"
 CWD=${GEM_HOME}${name}/
-#DATABASE_PATH=${CWD}validation.db
+
 DATABASE_PATH=/data/SBCS-BuggsLab/Josiah/DNA_Duplications/GeMoMa-1.5/cds-parts.fasta
 INPUT_FASTA_FILE=${CWD}gffread_filtered_protein.fasta
 BLAST_TAB_FILE=hits.csv
 genevalidator=/data2/SBCS-BuggsLab/Josiah/genevalidator-1.7.2-linux-x86_64/bin/genevalidator
 cd ${CWD}
 rm -r gffread_filtered_protein.fasta.html
-#sed 's/prediction/mRNA/g' filtered_predictions.gff > filtered_predictions_EDITED.gff
-#gffread filtered_predictions_EDITED.gff -g ${genome} -y gffread_filtered_protein.fasta
+
+sed 's/prediction/mRNA/g' filtered_predictions.gff > filtered_predictions_EDITED.gff
+gffread filtered_predictions_EDITED.gff -g ${genome} -y gffread_filtered_protein.fasta
 #/data2/SBCS-BuggsLab/Josiah/genevalidator-1.7.2-linux-x86_64/bin/makeblastdb -in /data/SBCS-BuggsLab/Josiah/DNA_Duplications/GeMoMa-1.5/cds-parts.fasta -dbtype prot -parse_seqids
 
 #This command actually works
-$genevalidator -d $DATABASE_PATH -num_threads $NUM_THREADS $INPUT_FASTA_FILE
-
-#/data2/SBCS-BuggsLab/Josiah/genevalidator-1.7.2-linux-x86_64/bin/makeblastdb
-
-#$genevalidator -d /data/SBCS-BuggsLab/Josiah/DNA_Duplications/GeMoMa-1.5/gene_validator/cds-parts.fasta -n 8 /data/SBCS-BuggsLab/Josiah/DNA_Duplications/GeMoMa-1.5/${name}/gffread_filtered_protein.fasta
+$genevalidator -d $DATABASE_PATH --num_threads=$NUM_THREADS $INPUT_FASTA_FILE
+#/data2/SBCS-BuggsLab/Josiah/genevalidator-1.7.2-linux-x86_64/bin/genevalidator ../FRAX34/gffread_filtered_protein.fasta -d /data/SBCS-BuggsLab/Josiah/DNA_Duplications/GeMoMa-1.5/cds-parts.fasta -num_threads=1
+# $genevalidator -d /data/SBCS-BuggsLab/Josiah/DNA_Duplications/GeMoMa-1.5/gene_validator/cds-parts.fasta -n 8 /data/SBCS-BuggsLab/Josiah/DNA_Duplications/GeMoMa-1.5/${name}/gffread_filtered_protein.fasta
 
 
 # Run BLAST (tabular output)
-#/data2/SBCS-BuggsLab/Josiah/genevalidator-1.7.2-linux-x86_64/bin/blastp -db $DATABASE_PATH -num_threads $NUM_THREADS -outfmt '7 qseqid sseqid sacc slen qstart qend sstart send length qframe pident nident evalue qseq sseq' -out $BLAST_TAB_FILE -query $INPUT_FASTA_FILE
+#/data2/SBCS-BuggsLab/Josiah/genevalidator-1.7.2-linux-x86_64/bin/blastp -db $DATABASE_PATH -num_threads=$NUM_THREADS -outfmt '7 qseqid sseqid sacc slen qstart qend sstart send length qframe pident nident evalue qseq sseq' -out $BLAST_TAB_FILE -query $INPUT_FASTA_FILE
 
 # Optional: Generate a fasta file for the BLAST hits.
 # Note: this works best if you use the same database used to create the BLAST OUTPUT file.
-#$genevalidator -d $DATABASE_PATH -e -t $BLAST_TAB_FILE -o 'qseqid sseqid sacc slen qstart qend sstart send length qframe pident nident evalue qseq sseq'
+# $genevalidator -d $DATABASE_PATH -e -t $BLAST_TAB_FILE -o 'qseqid sseqid sacc slen qstart qend sstart send length qframe pident nident evalue qseq sseq'
 
 # Run GeneValidator
 ## If you ran the previous command (i.e. if you produced fasta file for the BLAST hits)
@@ -51,5 +51,4 @@ $genevalidator -d $DATABASE_PATH -num_threads $NUM_THREADS $INPUT_FASTA_FILE
 #genevalidator -n $NUM_THREADS -t $BLAST_TAB_FILE -o 'qseqid sseqid sacc slen qstart qend sstart send length qframe pident nident evalue qseq sseq' -r $RAW_SEQUENCES_FILE $INPUT_FASTA_FILE
 
 ## If you did not generate the BLAST hits fasta file (this will automatically run the previous command for you)
-#$genevalidator -d $DATABASE_PATH -n $NUM_THREADS -t $BLAST_TAB_FILE -o 'qseqid sseqid sacc slen qstart qend sstart send length qframe pident nident evalue qseq sseq' $INPUT_FASTA_FILE
-
+# $genevalidator -d $DATABASE_PATH -n $NUM_THREADS -t $BLAST_TAB_FILE -o 'qseqid sseqid sacc slen qstart qend sstart send length qframe pident nident evalue qseq sseq' $INPUT_FASTA_FILE
--- GeMoMa-1.5/qsub_batch_all_gemoma.sh
+++ GeMoMa-1.5/qsub_batch_all_gemoma.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
+#$ -cwd              		 # use current working directory
+#$ -V                		 # this makes it verbose
+#$ -m aes
+#$ -M josiah.seaman@gmail.com
+#$ -j y				 		 # and put all output (inc errors) into it
+#$ -pe smp 4         		 # Request CPU cores
+#$ -l h_rt=72:0:0	 		 # Request hours runtime (upto 240 hours)
+#$ -l h_vmem=1G      		 # Request GB RAM / core, total= cores * vmem
+#$ -t 1-28
+#$ -tc 4                       #Max 4 concurrent.  Eyeballing disk IO
 ASSEMBLY="/data/SBCS-BuggsLab/Fraxinus_scaffolded_assemblies/"
 export ASSEMBLY="/data/SBCS-BuggsLab/Fraxinus_scaffolded_assemblies/"
 GEM_HOME="/data2/SBCS-BuggsLab/Josiah/DNA_Duplications/GeMoMa-1.5/"
+
+INPUT_LINE=$(sed -n "${SGE_TASK_ID}p" task_list.csv)
+${GEM_HOME}/gene_validator/validator_qsub.sh $INPUT_LINE
+
 #"FRAX30", "/data2/SBCS-BuggsLab/Fraxinus_scaffolded_assemblies/FRAX30_CLC_SSPACE_GAPCLOSER.fasta"
 #Gemoma doesn't like symlinks
 #qsub annotation_job.sh FRAX09 ${ASSEMBLY}F_pennsylvanica_Feb_2018_all_files/Fraxinus_pennsylvanica_20Feb2018_lPmM4.fasta
@@ -45,34 +59,34 @@ GEM_HOME="/data2/SBCS-BuggsLab/Josiah/DNA_Duplications/GeMoMa-1.5/"
 #./translation_from_annotation.sh FRAX34 ${ASSEMBLY}FRAX34_CLC_SSPACE_GAPCLOSER.fasta
 
 
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX01 ${ASSEMBLY}FRAX01_800bp_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX02 ${ASSEMBLY}FRAX02_800bp_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX03 ${ASSEMBLY}FRAX03_800bp_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX04 ${ASSEMBLY}FRAX04_800bp_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX05 ${ASSEMBLY}FRAX05_800bp_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX06 ${ASSEMBLY}FRAX06_800bp_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX07 ${ASSEMBLY}FRAX07_800bp_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX08 ${ASSEMBLY}FRAX08_800bp_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX11 ${ASSEMBLY}FRAX11_800bp_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX12 ${ASSEMBLY}FRAX12_800bp_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX13 ${ASSEMBLY}FRAX13_800bp_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX14 ${ASSEMBLY}FRAX14_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX15 ${ASSEMBLY}FRAX15_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX16 ${ASSEMBLY}FRAX16_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX19 ${ASSEMBLY}FRAX19_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX20 ${ASSEMBLY}FRAX20_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX21 ${ASSEMBLY}FRAX21_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX23 ${ASSEMBLY}FRAX23_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX25 ${ASSEMBLY}FRAX25_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX26 ${ASSEMBLY}FRAX26_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX27 ${ASSEMBLY}FRAX27_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX28 ${ASSEMBLY}FRAX28_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX29 ${ASSEMBLY}FRAX29_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX30 ${ASSEMBLY}FRAX30_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX31 ${ASSEMBLY}FRAX31_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX32 ${ASSEMBLY}FRAX32_CLC_SSPACE_GAPCLOSER.fasta
-#qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX33 ${ASSEMBLY}FRAX33_CLC_SSPACE_GAPCLOSER.fasta
-qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX34 ${ASSEMBLY}FRAX34_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX01 ${ASSEMBLY}FRAX01_800bp_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX02 ${ASSEMBLY}FRAX02_800bp_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX03 ${ASSEMBLY}FRAX03_800bp_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX04 ${ASSEMBLY}FRAX04_800bp_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX05 ${ASSEMBLY}FRAX05_800bp_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX06 ${ASSEMBLY}FRAX06_800bp_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX07 ${ASSEMBLY}FRAX07_800bp_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX08 ${ASSEMBLY}FRAX08_800bp_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX11 ${ASSEMBLY}FRAX11_800bp_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX12 ${ASSEMBLY}FRAX12_800bp_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX13 ${ASSEMBLY}FRAX13_800bp_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX14 ${ASSEMBLY}FRAX14_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX15 ${ASSEMBLY}FRAX15_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX16 ${ASSEMBLY}FRAX16_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX19 ${ASSEMBLY}FRAX19_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX20 ${ASSEMBLY}FRAX20_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX21 ${ASSEMBLY}FRAX21_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX23 ${ASSEMBLY}FRAX23_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX25 ${ASSEMBLY}FRAX25_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX26 ${ASSEMBLY}FRAX26_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX27 ${ASSEMBLY}FRAX27_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX28 ${ASSEMBLY}FRAX28_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX29 ${ASSEMBLY}FRAX29_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX30 ${ASSEMBLY}FRAX30_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX31 ${ASSEMBLY}FRAX31_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX32 ${ASSEMBLY}FRAX32_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX33 ${ASSEMBLY}FRAX33_CLC_SSPACE_GAPCLOSER.fasta
+#${GEM_HOME}/gene_validator/validator_qsub.sh FRAX34 ${ASSEMBLY}FRAX34_CLC_SSPACE_GAPCLOSER.fasta
 #translation_from_annotation.sh FRAX09 ${ASSEMBLY}F_pennsylvanica_Feb_2018_all_files/Fraxinus_pennsylvanica_20Feb2018_lPmM4.fasta
 
 #FRAX09 = Pennsylvanica
@@ -81,7 +95,6 @@ qsub ${GEM_HOME}/gene_validator/validator_qsub.sh FRAX34 ${ASSEMBLY}FRAX34_CLC_S
 #FRAX18 - unsequenced polyploid
 #FRAX22 - unsequenced polyploid
 #FRAX24 - unsequenced polyploid
-
 
 
 
