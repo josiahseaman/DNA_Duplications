77f6f9f603bf4929f6cb3fa24a8bbaf529804cd0
Josiah Seaman <josiah@newline.us>
2018-06-19 13:52:01 +0100

    Orthofinder script launch from DNA_Duplicaitons/

---
+++ orthofinder.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#$ -cwd                          # use current working directory
+#$ -V                            # this makes it verbose
+#$ -m aes
+#$ -M josiah.seaman@gmail.com
+#$ -j y                                          # and put all output (inc errors) into it
+#$ -pe smp 16                     # Request CPU cores
+#$ -l h_rt=24:0:0                         # Request hours runtime (upto 240 hours)
+#$ -l h_vmem=2G                 # Request GB RAM / core, total= cores * vmem
+NUM_THREADS=16
+source /data/SBCS-BuggsLab/Josiah/symmetry_env/bin/activate
+module load blast+
+/data2/SBCS-BuggsLab/Josiah/OrthoFinder-2.2.6_source/orthofinder.py \
+-f /data/SBCS-BuggsLab/Josiah/DNA_Duplications/Ash_Proteome -t $NUM_THREADS -S diamond
