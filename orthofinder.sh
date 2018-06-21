bd3ea7871061f117bf583c34e2569aecd962a79a
Josiah Seaman <btx142@frontend8.apocrita>
2018-06-21 16:39:04 +0100

    Apocrita side: removed old files

--- orthofinder.sh
+++ orthofinder.sh
@@ -4,10 +4,10 @@
 #$ -m aes
 #$ -M josiah.seaman@gmail.com
 #$ -j y                                          # and put all output (inc errors) into it
-#$ -pe smp 16                     # Request CPU cores
-#$ -l h_rt=24:0:0                         # Request hours runtime (upto 240 hours)
+#$ -pe smp 8                     # Request CPU cores
+#$ -l h_rt=144:0:0                         # Request hours runtime (upto 240 hours)
 #$ -l h_vmem=2G                 # Request GB RAM / core, total= cores * vmem
-NUM_THREADS=16
+NUM_THREADS=8
 source /data/SBCS-BuggsLab/Josiah/symmetry_env/bin/activate
 module load blast+
 /data2/SBCS-BuggsLab/Josiah/OrthoFinder-2.2.6_source/orthofinder.py \
