c8a2d8bd142c5df4a431c09645e4c0d656e849b8
Josiah Seaman <btx142@frontend8.apocrita>
2018-06-22 17:22:49 +0100

    import mafft

--- orthofinder.sh
+++ orthofinder.sh
@@ -6,10 +6,11 @@
 #$ -j y                                          # and put all output (inc errors) into it
 #$ -pe smp 8                     # Request CPU cores
 #$ -l h_rt=50:0:0                         # Request hours runtime (upto 240 hours)
-#$ -l h_vmem=2G                 # Request GB RAM / core, total= cores * vmem
+#$ -l h_vmem=6G                 # Request GB RAM / core, total= cores * vmem
 NUM_THREADS=8
 source /data/SBCS-BuggsLab/Josiah/symmetry_env/bin/activate
 module load blast+
+module load mafft
 #Original command lacking outgroups
 #/data2/SBCS-BuggsLab/Josiah/OrthoFinder-2.2.6_source/orthofinder.py -f /data/SBCS-BuggsLab/Josiah/DNA_Duplications/Ash_Proteome -t $NUM_THREADS -S diamond
 
