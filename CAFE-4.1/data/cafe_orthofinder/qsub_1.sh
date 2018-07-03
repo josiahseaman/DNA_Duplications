d50b19fd0dcd802a3f74bbb71f8464015b125f66
Josiah Seaman <btx142@frontend8.apocrita>
2018-07-03 10:47:50 +0100

    Added CAFE into repo with data

---
+++ CAFE-4.1/data/cafe_orthofinder/qsub_1.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#$ -cwd           # Set the working directory for the job to the current directory
+#$ -V
+#$ -j y           # Merge the standard output and standard error
+#$ -m aes
+#$ -M josiah.seaman@gmail.com
+#$ -l h_rt=01:0:0 # Request runtime
+#$ -pe smp 8      # Request 1 CPU cores
+#$ -l h_vmem=200M   # Request GB RAM per core
+
+source /data/SBCS-BuggsLab/Josiah/symmetry_env/bin/activate
+cd /data/SBCS-BuggsLab/Josiah/DNA_duplications/CAFE-4.1/data/cafe_prototype
+# python ../cafe_tutorial/python_scripts/cafetutorial_prep_r8s.py -i unscaled_species.tre -o r8s_ctl_file.txt -s 35157236 -p 'FEXC,Olea' -c '25'
+# python ../cafe_tutorial/python_scripts/cafetutorial_clade_and_size_filter.py -i Cafe_orthofinder_Orthogroups.GeneCount.csv -o filtered_OG_counts.txt -s
+
+
+cafe /data2/SBCS-BuggsLab/Josiah/DNA_duplications/CAFE-4.1/data/cafe_prototype/run1_orthofinder_full.sh
