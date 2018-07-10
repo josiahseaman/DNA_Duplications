d2b137b48b6811049624be6a4e76e4528dd41051
Josiah Seaman <josiah@newline.us>
2018-07-10 16:16:35 +0100

    r8s files with calibration points

--- CAFE-4.1/data/cafe_orthofinder/qsub_1.sh
+++ CAFE-4.1/data/cafe_orthofinder/qsub_1.sh
@@ -10,8 +10,11 @@
 
 source /data/SBCS-BuggsLab/Josiah/symmetry_env/bin/activate
 cd /data/SBCS-BuggsLab/Josiah/DNA_duplications/CAFE-4.1/data/cafe_prototype
-# python ../cafe_tutorial/python_scripts/cafetutorial_prep_r8s.py -i unscaled_species.tre -o r8s_ctl_file.txt -s 35157236 -p 'FEXC,Olea' -c '25'
-# python ../cafe_tutorial/python_scripts/cafetutorial_clade_and_size_filter.py -i Cafe_orthofinder_Orthogroups.GeneCount.csv -o filtered_OG_counts.txt -s
+python ../cafe_tutorial/python_scripts/cafetutorial_prep_r8s.py \
+-i unscaled_species.tre -o r8s_ctl_file.txt -s 763103 \
+-p 'FRAX00,Mguttatus FRAX00,Slycopersicum FRAX00,Oeuropea FRAX11,FRAX07 FRAX01,FRAX07 FRAX11,FRAX14'
+-c '87 79 36 19 14 11'
 
+#python ../cafe_tutorial/python_scripts/cafetutorial_clade_and_size_filter.py -i Cafe_orthofinder_Orthogroups.GeneCount.csv -o filtered_OG_counts.txt -s
 
-cafe /data2/SBCS-BuggsLab/Josiah/DNA_duplications/CAFE-4.1/data/cafe_prototype/run1_orthofinder_full.sh
+#cafe /data2/SBCS-BuggsLab/Josiah/DNA_duplications/CAFE-4.1/data/cafe_prototype/run1_orthofinder_full.sh
