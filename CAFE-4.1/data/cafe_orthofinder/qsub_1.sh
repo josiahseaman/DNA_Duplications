c1611901900b4f9d01faadf9f756debaf0548b14
Josiah Seaman <josiah@newline.us>
2018-07-10 16:57:58 +0100

    fixed formatting for r8s calibration points

--- CAFE-4.1/data/cafe_orthofinder/qsub_1.sh
+++ CAFE-4.1/data/cafe_orthofinder/qsub_1.sh
@@ -12,8 +12,8 @@ source /data/SBCS-BuggsLab/Josiah/symmetry_env/bin/activate
 cd /data/SBCS-BuggsLab/Josiah/DNA_duplications/CAFE-4.1/data/cafe_orthofinder
 python ../cafe_tutorial/python_scripts/cafetutorial_prep_r8s.py \
 -i RAxML_result__rearranged_to_lauras_order.tre -o r8s_Frax_timetree_script.txt -s 763103 \
--p '"FRAX00,Mguttatus" "FRAX00,Slycopersicum" "FRAX00,Oeuropea" "FRAX11,FRAX07" "FRAX01,FRAX07" "FRAX11,FRAX14"' \
--c '87 79 36 19 14 11'
+-p "FRAX00,Mguttatus FRAX00,Slycopersicum FRAX00,Oeuropea FRAX11,FRAX07 FRAX01,FRAX07 FRAX11,FRAX14" \
+-c="87,79,36,19,14,11"
 
 #python ../cafe_tutorial/python_scripts/cafetutorial_clade_and_size_filter.py -i Cafe_orthofinder_Orthogroups.GeneCount.csv -o filtered_OG_counts.txt -s
 
