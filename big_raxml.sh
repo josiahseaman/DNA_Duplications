b424ec57562e1497eb3055dea9fadab3394403ac
Josiah Seaman <btx142@nxv32.apocrita>
2018-07-10 16:16:47 +0100

    big_raxml.sh

--- big_raxml.sh
+++ big_raxml.sh
@@ -6,12 +6,13 @@
 #$ -j y                                          # and put all output (inc errors) into it
 #$ -pe smp 8                     # Request CPU cores
 #$ -l h_rt=50:0:0                         # Request hours runtime (upto 240 hours)
-#$ -l h_vmem=2G                 # Request GB RAM / core, total= cores * vmem
+#$ -l h_vmem=5G                 # Request GB RAM / core, total= cores * vmem
 NUM_THREADS=8
 source /data/SBCS-BuggsLab/Josiah/symmetry_env/bin/activate
 module load blast+
 module load mafft
 module load raxml
+alias raxml=raxmlHPC
 
 #Original command lacking outgroups
 #/data2/SBCS-BuggsLab/Josiah/OrthoFinder-2.2.6_source/orthofinder.py -f /data/SBCS-BuggsLab/Josiah/DNA_Duplications/Ash_Proteome -t $NUM_THREADS -S diamond -T raxml
@@ -24,7 +25,7 @@ module load raxml
 #Now force our species tree for the Ortholog identification
 /data2/SBCS-BuggsLab/Josiah/OrthoFinder-2.2.6_source/orthofinder.py \
 -fg /data/SBCS-BuggsLab/Josiah/DNA_Duplications/Ash_Proteome/Results_Jun25 \
--s /data/SBCS-BuggsLab/Josiah/DNA_Duplications/data/Primary_concordance_tree_with_sample_CFs_full_length_sequences_all_taxa.tre \
+-s /data/SBCS-BuggsLab/Josiah/DNA_Duplications/CAFE-4.1/data/cafe_orthofinder/Primary_concordance__short_names.tre \
 -t $NUM_THREADS -S diamond -M msa -T raxml
 
 
