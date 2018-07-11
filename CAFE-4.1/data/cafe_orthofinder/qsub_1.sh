92fd34ebead94145622c2d6342c27c063d227a36
Josiah Seaman <josiah@newline.us>
2018-07-11 14:22:07 +0100

    Trying to fix the tree by expanding small branch length (0.04) and making ultrametric

--- CAFE-4.1/data/cafe_orthofinder/qsub_1.sh
+++ CAFE-4.1/data/cafe_orthofinder/qsub_1.sh
@@ -15,6 +15,12 @@ cd /data/SBCS-BuggsLab/Josiah/DNA_Duplications/CAFE-4.1/data/cafe_orthofinder
 #-p "FRAX00,Mguttatus FRAX00,Slycopersicum FRAX00,Oeuropea FRAX11,FRAX07 FRAX01,FRAX07 FRAX11,FRAX14" \
 #-c="87,79,36,19,14,11"
 
-#python ../cafe_tutorial/python_scripts/cafetutorial_clade_and_size_filter.py -i Cafe_orthofinder_Orthogroups.GeneCount.csv -o filtered_OG_counts.txt -s
-#shuf -n 1000 filtered_OG_counts.txt > Random_Sample_1000.GeneCount.txt
+#r8s -b -f r8s_Frax_timetree_script.txt > r8s.tmp
+#tail -n 1 r8s.tmp  | cut -c 16- > Frax_species_tree_ultrametric.tre
+
+python ../cafe_tutorial/python_scripts/cafetutorial_clade_and_size_filter.py -i Cafe_orthofinder_Orthogroups.GeneCount.csv -o filtered_OG_counts.txt -s
+
+head filtered_OG_counts.txt -n 1 > Random_Sample_1000.GeneCount.txt  # important to add header
+shuf -n 1000 filtered_OG_counts.txt >> Random_Sample_1000.GeneCount.txt
+
 cafe /data2/SBCS-BuggsLab/Josiah/DNA_Duplications/CAFE-4.1/data/cafe_orthofinder/run_cafe_orthofinder.sh
