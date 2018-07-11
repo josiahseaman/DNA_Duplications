4e9d777b78b63c2d02806f77e5a21e0228ff77ff
Josiah Seaman <btx142@frontend8.apocrita>
2018-07-11 10:00:37 +0100

    r8s run

--- CAFE-4.1/data/cafe_orthofinder/qsub_1.sh
+++ CAFE-4.1/data/cafe_orthofinder/qsub_1.sh
@@ -9,12 +9,12 @@
 #$ -l h_vmem=200M   # Request GB RAM per core
 
 source /data/SBCS-BuggsLab/Josiah/symmetry_env/bin/activate
-cd /data/SBCS-BuggsLab/Josiah/DNA_duplications/CAFE-4.1/data/cafe_orthofinder
+cd /data/SBCS-BuggsLab/Josiah/DNA_Duplications/CAFE-4.1/data/cafe_orthofinder
 python ../cafe_tutorial/python_scripts/cafetutorial_prep_r8s.py \
--i RAxML_result__rearranged_to_lauras_order.tre -o r8s_Frax_timetree_script.txt -s 763103 \
+-i RAxML_result__rearranged_to_lauras_order.tre -o r8s_Frax_timetree_script.txt -s 25182399 \
 -p "FRAX00,Mguttatus FRAX00,Slycopersicum FRAX00,Oeuropea FRAX11,FRAX07 FRAX01,FRAX07 FRAX11,FRAX14" \
 -c="87,79,36,19,14,11"
 
 #python ../cafe_tutorial/python_scripts/cafetutorial_clade_and_size_filter.py -i Cafe_orthofinder_Orthogroups.GeneCount.csv -o filtered_OG_counts.txt -s
 
-#cafe /data2/SBCS-BuggsLab/Josiah/DNA_duplications/CAFE-4.1/data/cafe_prototype/run1_orthofinder_full.sh
+#cafe /data2/SBCS-BuggsLab/Josiah/DNA_duplications/CAFE-4.1/data/cafe_orthofinder/run1_cafe_orthofinder_full.sh
