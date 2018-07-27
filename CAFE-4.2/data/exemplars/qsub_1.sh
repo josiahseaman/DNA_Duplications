36bcd27176cc9013cb0490235d795adbf2de981b
Josiah Seaman <josiah@newline.us>
2018-07-27 11:56:24 +0100

    run_internal_node_test script

--- CAFE-4.2/data/exemplars/qsub_1.sh
+++ CAFE-4.2/data/exemplars/qsub_1.sh
@@ -18,13 +18,15 @@ cd /data/SBCS-BuggsLab/Josiah/DNA_Duplications/CAFE-4.2/data/exemplars
 #r8s -b -f r8s_Frax_timetree_script.txt > r8s.tmp
 #tail -n 1 r8s.tmp  | cut -c 16- > Frax_species_tree_ultrametric.tre
 
-#python ../cafe_tutorial/python_scripts/cafetutorial_clade_and_size_filter.py -i Cafe_orthofinder_Orthogroups.GeneCount.csv -o filtered_OG_counts.txt -s
+python ../cafe_tutorial/python_scripts/cafetutorial_clade_and_size_filter.py \
+-i exemplar_filtered_OG_counts.txt -o exemplar_clade_filter.csv -s
 
 #head filtered_OG_counts.txt -n 1 > Random_Sample_1000.GeneCount.txt  # important to add header
 #shuf -n 1000 filtered_OG_counts.txt >> Random_Sample_1000.GeneCount.txt
 
-cafe /data/SBCS-BuggsLab/Josiah/DNA_Duplications/CAFE-4.2/data/exemplars/run_cafe_orthofinder.sh
+cafe /data/SBCS-BuggsLab/Josiah/DNA_Duplications/CAFE-4.2/data/exemplars/run_internal_node_test.sh
 
 #summarize results with python2.7
 #IMPORTANT: you need to input the name of the current result file
-python /data/SBCS-BuggsLab/Josiah/DNA_Duplications/CAFE-4.1/data/cafe_tutorial/python_scripts/cafetutorial_report_analysis.py -i reports/exemplars_reestimate.txt -o reports/latest_summary
+REPORT_SCRIPT=/data/SBCS-BuggsLab/Josiah/DNA_Duplications/CAFE-4.1/data/cafe_tutorial/python_scripts/cafetutorial_report_analysis.py
+python $REPORT_SCRIPT -i reports/internal_node_test.cafe -o reports/internal_node_test_summary
---
+++ CAFE-4.2/data/exemplars/run_internal_node_test.sh
@@ -0,0 +1,7 @@
+#!cafe
+load -i exemplar_clade_filter.csv -t 8 -l reports/internal_node_test.log
+#Approximated ultrametric based on 763103 sites and 5 calibration points
+#A NEWICK-formatted tree containing branch lengths and taxon names as they are specified in the \ref Load "input file". Branch lengths should be integer units and the tree should be ultrametric (all paths from root to tip should have the same length). If the tree is not ultrametric to a tolerance of .01%, a warning will be logged. Please note that there should be no spaces in the tree string, nor semicolons at the end of the line.
+tree ((((FRAX07:14.0,(FRAX00:10.0,FRAX06:10.0):4.0):5.0,((FRAX19:10.0,FRAX11:10.0):1.0,FRAX09:11.0):8.0):17.0,Oeuropea:36.0):43.0,(Slycopersicum:37.0,Mguttatus:37.0):42.0);
+lambdamu -s -t ((((1,(1,1)1)2,((1,1)1,1)1)1,3)1,(4,4)4);
+report reports/internal_node_test
