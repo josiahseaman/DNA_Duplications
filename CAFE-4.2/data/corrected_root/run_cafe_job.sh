963d5622e503f39b454b1834be12b1809294bf56
Josiah Seaman <josiah@newline.us>
2018-09-20 11:25:44 +0100

    Corrected root, first steps in new script.  New species tree.

---
+++ CAFE-4.2/data/corrected_root/run_cafe_job.sh
@@ -0,0 +1,9 @@
+cafe corrected_root_JSG.cafe
+#IMPORTANT: you need to input the name of the current result file
+REPORT_SCRIPT=/data/SBCS-BuggsLab/Josiah/DNA_Duplications/CAFE-4.1/data/cafe_tutorial/python_scripts/cafetutorial_report_analysis.py
+python $REPORT_SCRIPT -i reports/corrected_root_JSG_l=0.00838.cafe -o reports/corrected_root_JSG_l=0.00838
+
+#cafe corrected_root_JSG.cafe
+##IMPORTANT: you need to input the name of the current result file
+#REPORT_SCRIPT=/data/SBCS-BuggsLab/Josiah/DNA_Duplications/CAFE-4.1/data/cafe_tutorial/python_scripts/cafetutorial_report_analysis.py
+#python $REPORT_SCRIPT -i reports/corrected_root_JSG.cafe -o reports/corrected_root_JSG
