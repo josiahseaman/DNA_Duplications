376b8ea972db7aab6d09c3e2b2593ae4a1cb3517
Josiah Seaman <btx142@frontend8.apocrita>
2018-10-16 13:56:05 +0100

    Starting on error model

--- CAFE-4.2/data/homeologs_only/caferror.sh
+++ CAFE-4.2/data/homeologs_only/caferror.sh
@@ -1,6 +1,4 @@
 #!/data/SBCS-BuggsLab/Josiah/tools/cafe
-load -i homeolog_counts.txt -t 8 -l reports/oleaceae_homeologs_one_rate.log
 tree ((((((((((FRAX30:2.0,FRAX32:2.0):1.0,FRAX28:3.0):2.0,FRAX12:5.0):4.0,(FRAX07:8.0,FRAX29:8.0):1.0):4.0,FRAX08:13.0):1.0,(((((FRAX01:2.0,FRAX16:2.0):4.0,FRAX15:6.0):2.0,FRAX00:8.0):2.0,(FRAX06:9.0,FRAX23:9.0):1.0):3.0,FRAX25:13.0):1.0):3.0,FRAX21:17.0):2.0,(((FRAX19:8.0,FRAX20:8.0):2.0,((FRAX11:5.0,FRAX27:5.0):4.0,FRAX04:9.0):1.0):1.0,(((((FRAX03:1.0,FRAX09:1.0):1.0,FRAX13:2.0):2.0,(FRAX26:2.0,FRAX14:2.0):2.0):3.0,FRAX05:7.0):2.0,FRAX33:9.0):2.0):8.0):15.0,FRAX31:34.0):2.0,Oeuropea:36.0):36.0;
+load -i homeolog_counts.txt -t 10 -l reports/error_files/cafe_pre_log.txt
 lambdamu -l 0.00312885131707 -m 0.01967130811162
-lambdamu -s
-report reports/oleaceae_homeologs_one_rate
--- CAFE-4.2/data/homeologs_only/run_cafe_job.sh
+++ CAFE-4.2/data/homeologs_only/run_cafe_job.sh
@@ -15,8 +15,10 @@ REPORT_SCRIPT=/data/SBCS-BuggsLab/Josiah/DNA_Duplications/CAFE-4.1/data/cafe_tut
 #python $REPORT_SCRIPT -i reports/oleaceae_homeologs_l009_m015.cafe -o reports/oleaceae_homeologs_l009_m015
 
 #Actually search for ideal parameters QSUB
-cafe oleaceae_homeologs_one_rate.cafe
-python $REPORT_SCRIPT -i reports/oleaceae_homeologs_one_rate.cafe -o reports/oleaceae_homeologs_one_rate
+#cafe oleaceae_homeologs_one_rate.cafe
+#python $REPORT_SCRIPT -i reports/oleaceae_homeologs_one_rate.cafe -o reports/oleaceae_homeologs_one_rate
 
+cd /data/SBCS-BuggsLab/Josiah/DNA_Duplications/CAFE-4.2/data/homeologs_only
+python caferror.py -i oleaceae_homeologs_l003_m019.cafe -d reports/error_files -e .08 -v 0
 
 ##IMPORTANT: you need to input the name of the current result file
