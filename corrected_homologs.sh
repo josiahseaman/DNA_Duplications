5857f3d38b6e4b91d7979df50e4fd226aae8bbac
Josiah Seaman <btx142@frontend11.apocrita>
2019-04-04 15:59:25 +0100

    Corrected_homologs restarting without 02 and 34

--- corrected_homologs.sh
+++ corrected_homologs.sh
@@ -4,13 +4,17 @@
 #$ -m aes
 #$ -M josiah.seaman@gmail.com
 #$ -j y                                          # and put all output (inc errors) into it
-#$ -pe smp 3                     # Request CPU cores
-#$ -l h_rt=8:0:0                         # Request hours runtime (upto 240 hours)
-#$ -l h_vmem=1G                 # Request GB RAM / core, total= cores * vmem
-NUM_THREADS=3
+#$ -pe smp 4                     # Request CPU cores
+#$ -l h_rt=16:0:0                         # Request hours runtime (upto 240 hours)
+#$ -l h_vmem=4G                 # Request GB RAM / core, total= cores * vmem
+NUM_THREADS=4
 source /data/SBCS-BuggsLab/Josiah/symmetry_env/bin/activate
+module load blast+
+module load mafft
 #module load singularity
 #alias raxml="singularity run /data/containers/raxml-ng/raxml-ng raxml-ng"
+module load raxml
+#alias raxml=raxmlHPC
 
 #Original command lacking outgroups
 #/data2/SBCS-BuggsLab/Josiah/OrthoFinder-2.2.6_source/orthofinder.py -f /data/SBCS-BuggsLab/Josiah/DNA_Duplications/Ash_Proteome -t $NUM_THREADS -S diamond -T raxml
@@ -25,9 +29,10 @@ source /data/SBCS-BuggsLab/Josiah/symmetry_env/bin/activate
 #-fg /data/SBCS-BuggsLab/Josiah/DNA_Duplications/Ash_Proteome/Results_Jun25 \
 #-s /data/SBCS-BuggsLab/Josiah/DNA_Duplications/CAFE-4.1/data/cafe_orthofinder/Primary_concordance__short_names.tre \
 #-t $NUM_THREADS -S diamond -M msa -T raxml
+#-f /data/SBCS-BuggsLab/Josiah/DNA_Duplications/Ash_Proteome \
 
 #Using corrected species tree with correct outgroup rooting
 /data/SBCS-BuggsLab/Josiah/OrthoFinder-2.2.6_source/orthofinder.py \
---from-trees /data/SBCS-BuggsLab/Josiah/DNA_Duplications/Ash_Proteome/Results_Jun25/Orthologues_Jul04_2/ \
--s /data/SBCS-BuggsLab/Josiah/DNA_Duplications/CAFE-4.2/data/corrected_orthologs/Species_tree_corrected_root_added_02_34.tre \
--t 3
+-b /data/SBCS-BuggsLab/Josiah/DNA_Duplications/Ash_Proteome/Results_Jun25/WorkingDirectory \
+-s /data/SBCS-BuggsLab/Josiah/DNA_Duplications/CAFE-4.2/data/corrected_orthologs/Species_tree_corrected_root_without_02_34.tre \
+-t $NUM_THREADS -S diamond -M msa -T raxml 
